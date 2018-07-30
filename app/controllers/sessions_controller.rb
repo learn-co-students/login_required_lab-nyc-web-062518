ee8c5c9e8e8499a31169834e38d678fedc599f63
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-07-29 21:50:20 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if !params[:name] || params[:name].empty?
+      redirect_to :login
+    else
+      session[:name] = params[:name]
+      redirect_to :root
+    end
+  end
+
+  def destroy
+      session.delete :name
+      redirect_to :login
+  end
+
+  def require_login
+    return head(:forbidden) unless session.include? :name
+  end
+
+
+end
